e0570c22f55c27602408274de9e66ffac0ec8e57
Luis Lopez <luis.lopezespinosa@colorado.edu>
2015-09-17 19:45:06 -0500

    adds Vagrantfile and fabric deployment

---
+++ Vagrantfile
@@ -0,0 +1,38 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.provider :virtualbox do |vb, override|
+    override.vm.box = "ubuntu/trusty64"
+    override.vm.box_url =  "https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/14.04/providers/virtualbox.box"
+    override.vm.synced_folder ".", "/vagrant",type: "rsync"
+    override.ssh.username = "vagrant"
+    vb.customize ["modifyvm", :id, "--cpus", "1"]
+    vb.customize ["modifyvm", :id, "--memory", "512"]
+  end
+
+  config.vm.provider :aws do |aws, override|
+    override.vm.box = "Restparql"
+    override.vm.box_url = "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
+    override.ssh.username = "ubuntu"
+    override.ssh.private_key_path = "#{ENV['AWS_KEYPATH']}"
+
+    aws.access_key_id = "#{ENV['AWS_ACCESS_KEY']}"
+    aws.secret_access_key = "#{ENV['AWS_SECRET_KEY']}"
+    aws.keypair_name = "#{ENV['AWS_KEYPAIR_NAME']}"
+    aws.region = "us-west-2"
+    aws.ami    = "ami-870a2fb7" # Ubuntu 14.04 LTS x64
+    aws.instance_type = "t2.micro"
+    aws.tags = {
+      'instance' => 'bcube-restparql',
+    }
+  end
+
+  config.vm.provision :fabric do |fabric|
+    fabric.fabfile_path = "./fabfile.py"
+    fabric.tasks = ["provision", ]
+  end
+
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+end
---
+++ fabfile.py
@@ -0,0 +1,104 @@
+from fabric.api import cd, sudo
+from fabric.contrib.files import exists
+
+
+##############
+### config ###
+##############
+
+git_repo = 'https://github.com/b-cube/restparql.git'
+app_dir = '/opt/restparql'
+virtualenv_dir = app_dir + '/env'
+flask_dir = app_dir + '/app'
+nginx_dir = '/etc/nginx/sites-enabled'
+supervisor_dir = '/etc/supervisor/conf.d'
+
+
+#############
+### tasks ###
+#############
+
+def install_os_requirements_1():
+    """ Install required packages. """
+    sudo('apt-get update')
+    sudo('apt-get install -y build-essential')
+    sudo('apt-get install -y python3.4-dev')
+    sudo('apt-get install -y python-pip')
+    sudo('apt-get install -y nginx')
+    sudo('apt-get install -y supervisor')
+    sudo('apt-get install -y git')
+    sudo('sudo pip install virtualenv')
+
+def checkout_project_2():
+    """
+    1. Create project directories
+    2. checkout the latest code
+    """
+    if exists(app_dir) is False:
+        sudo('mkdir ' + app_dir)
+    if exists(flask_dir) is False:
+        with cd(app_dir):
+            sudo('git clone ' + git_repo + ' ' + flask_dir)
+    else:
+        with cd(flask_dir):
+            sudo('git pull --rebase')
+
+
+def install_virtualenv_packages_3():
+    """
+    1. Create a new virtualenv
+    2. Install the requirements
+    """
+    if exists(virtualenv_dir) is False:
+        sudo('virtualenv -p python3 ' + virtualenv_dir)
+    sudo('source ' + virtualenv_dir + '/bin/activate')
+    sudo('pip install --upgrade pip')
+    sudo('pip install -r ' + flask_dir + '/requirements.txt')
+
+
+def configure_nginx_4():
+    """
+    1. Remove default nginx config file
+    2. Create new config file
+    3. Setup new symbolic link
+    4. Restart nginx
+    """
+    sudo('/etc/init.d/nginx start')
+    if exists('/etc/nginx/sites-enabled/default'):
+        sudo('rm /etc/nginx/sites-enabled/default')
+    if exists('/etc/nginx/sites-enabled/nginx_flask') is False:
+        sudo('touch /etc/nginx/sites-available/nginx_flask')
+        sudo('ln -s /etc/nginx/sites-available/nginx_flask' +
+             ' /etc/nginx/sites-enabled/restparql')
+    sudo('cp ' + flask_dir + '/config/nginx_flask ' + nginx_dir)
+    sudo('/etc/init.d/nginx restart')
+
+
+def configure_supervisor_5():
+    """
+    1. update supervisor config file
+    2. update supervisor
+    """
+    sudo('cp ' + flask_dir + '/config/restparql.conf ' +
+         '/etc/supervisor/conf.d/')
+    sudo('supervisorctl reread')
+    sudo('supervisorctl update')
+
+
+def run_app():
+    """ Run the app! """
+    with cd(flask_dir):
+        sudo('supervisorctl start restparql')
+
+
+def status():
+    """ Is our app live? """
+    sudo('supervisorctl status')
+
+
+def provision():
+    install_os_requirements_1()
+    checkout_project_2()
+    install_virtualenv_packages_3()
+    configure_nginx_4()
+    configure_supervisor_5()
